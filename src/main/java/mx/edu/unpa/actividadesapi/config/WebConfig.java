e78b526ce1328eb84fd499434f0f5d47a1ec7580
anntonio3 <antoniounpa@gmail.com>
2026-04-20 21:02:16 -0700

    Spring 1 - Backend terminado US-02-vianey

---
+++ src/main/java/mx/edu/unpa/actividadesapi/config/WebConfig.java
@@ -0,0 +1,33 @@
+package mx.edu.unpa.actividadesapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Carpeta absoluta donde se guardan las imágenes
+        Path uploadDir = Paths.get("uploads").toAbsolutePath().normalize();
+        String uploadPath = uploadDir.toUri().toString();
+
+        // Mapea cualquier petición a /uploads/** a la carpeta física
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations(uploadPath)
+                .setCachePeriod(3600);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/uploads/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET");
+    }
+
+}
