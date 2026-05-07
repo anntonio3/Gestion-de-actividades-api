3e4c6dc2b37be7a9cc4b66ed027c1314f9d0304f
anntonio3 <antoniounpa@gmail.com>
2026-05-07 14:59:18 -0600

    Primeros cambios

---
+++ src/main/java/mx/edu/unpa/actividadesapi/enums/RespuestaAsistencia.java
@@ -0,0 +1,7 @@
+package mx.edu.unpa.actividadesapi.enums;
+
+public enum RespuestaAsistencia {
+    VOY,
+    TAL_VEZ,
+    NO_VOY
+}
---
+++ src/main/java/mx/edu/unpa/actividadesapi/model/Visitante.java
@@ -0,0 +1,27 @@
+package mx.edu.unpa.actividadesapi.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "visitantes")
+public class Visitante {
+
+    // UUID generado por el backend, no por la BD.
+    @Id
+    @Column(name = "id_visitante", length = 36)
+    private String idVisitante;
+
+    @Column(name = "fecha_creacion", nullable = false, updatable = false)
+    private LocalDateTime fechaCreacion;
+
+    @PrePersist
+    protected void onCreate() {
+        this.fechaCreacion = LocalDateTime.now();
+    }
+}
