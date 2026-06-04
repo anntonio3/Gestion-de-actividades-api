f3b64fc68ca5c37c9a1c8c3dc70c6277bc133f63
Berenice Bustamante <bere01bust@gmail.com>
2026-06-03 20:20:11 -0600

    feat: se agrego imagenes a las solicitudes

--- src/main/java/mx/edu/unpa/actividadesapi/service/SolicitudesService.java
+++ src/main/java/mx/edu/unpa/actividadesapi/service/SolicitudesService.java
@@ -2,14 +2,20 @@ package mx.edu.unpa.actividadesapi.service;
 
 import mx.edu.unpa.actividadesapi.dto.request.ActualizarActividadRequestDTO;
 import mx.edu.unpa.actividadesapi.dto.response.SolicitudResponseDTO;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.util.List;
 
 public interface SolicitudesService {
-    // Interface antes llamada ActividadService
 
     List<SolicitudResponseDTO> getMisSolicitudes(Integer idProfesor, String estado);
 
-    // US-05: Editar una actividad en estado Pendiente
     SolicitudResponseDTO editarActividad(Integer idActividad, Integer idProfesor,
-                                          ActualizarActividadRequestDTO dto);
+                                         ActualizarActividadRequestDTO dto);
+
+    /** Reemplaza (o agrega si no había) la portada de la actividad. Solo una imagen permitida. */
+    SolicitudResponseDTO reemplazarImagen(Integer idActividad, Integer idProfesor, MultipartFile imagen);
+
+    /** Elimina la portada de la actividad sin subir otra. */
+    SolicitudResponseDTO eliminarImagen(Integer idActividad, Integer idProfesor);
 }
